3453fe8bef2509fec094c0e9b6a43b76967e9320
mutyalashailaja <mutyala.shailaja@gmail.com>
2016-08-27 07:39:35 +0530

    s4 asgn1

---
+++ src/ReverseOfArray.java
@@ -0,0 +1,25 @@
+
+public class ReverseOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]  a2;
+		a2= new int[10];
+	
+		for(int i=0;i<10;i++){
+			a2[i]=i;
+		System.out.println(a2[i]);
+		
+			}
+		System.out.println("--------------------------");
+		int i;
+		int[] a1;
+		a1=new int[10];
+		for(i=9;i>0;i--){
+			a1[i]=i;
+			
+		System.out.println(a1[i]);
+
+	}
+
+	}}
